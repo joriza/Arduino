feefda47cba5a18a3369ee1cb41808f15f35ecb1
Jorge J. IZAGUIRRE <jizaguirre@huenei.com>
2020-01-17 02:22:10 -0300

    200117

---
+++ pru03/app.ino
@@ -0,0 +1,17 @@
+#include "TM1637.h"
+#define CLK 3    
+#define DIO 2
+
+TM1637 Display1(CLK,DIO);
+int8_t Digits[] = 6,2,1,2};
+
+void setup()
+   {  
+       Display1.set();
+       Display1.init() ;
+   }
+
+void loop()
+   {
+        Display1.display(Digits);
+   }
