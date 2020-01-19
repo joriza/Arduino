f8a419c61fcf58573c77233c7249b92a4b32599c
Jorge J. IZAGUIRRE <jizaguirre@huenei.com>
2020-01-19 14:40:47 -0300

    200119-1440

--- pru_pulsa/pulsa01.ino
+++ pru_pulsa/pulsa01.ino
@@ -4,26 +4,39 @@
 */
 
 // Constantes para definir los pines
-const int pulsadorPin = 4;
-const int ledPin = 13;
+// Tener en cuenta que los pines fisicos no se corresponden con los digitales y cambian segun el modelo del arduino
+// P. Ej. Nano = I/O digital 4 = patafísica 7
+const int pulsIzq = 6; //Pulsador fisico 1 (izq a der)
+const int pulsDer = 5; //Pulsador fisico 2 
+const int ledIzq = 12; //Direccion izquireda
+const int ledDer = 11; //Direccion izquireda
 
 int valorPulsador = 0;
 
 void setup() {
     // Activamos los pines de entrada y salida
-    pinMode(pulsadorPin, INPUT);
-    pinMode(ledPin, OUTPUT);
+    pinMode(pulsIzq, INPUT_PULLUP); //Define D4
+    pinMode(pulsDer, INPUT_PULLUP); //Define D4
+    pinMode(ledIzq, OUTPUT);
+    pinMode(ledDer, OUTPUT);
 }
 
 void loop() {
-    // Leemos el valor del pin
-    valorPulsador = digitalRead(pulsadorPin);
+    // Leemos el valor de uno de los
+    valorPulsador = digitalRead(pulsIzq);
  
   // Encendemos o apagamos el pin del LED según convenga
   if (valorPulsador == HIGH) {
-      digitalWrite(ledPin, HIGH);
+      digitalWrite(ledIzq, LOW);
   }
   else {
-      digitalWrite(ledPin, LOW);
+      digitalWrite(ledIzq, HIGH);
+  }
+  // Encendemos o apagamos el pin del LED según convenga
+  if (digitalRead(pulsDer) == HIGH) {
+      digitalWrite(ledDer, LOW);
+  }
+  else {
+      digitalWrite(ledDer, HIGH);
   }
 }
