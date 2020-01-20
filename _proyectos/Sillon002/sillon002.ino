7d5a42a7b3c554d40261b69746349563b2cb97f5
Jorge J. IZAGUIRRE <jizaguirre@huenei.com>
2020-01-20 02:57:46 -0300

    200120-0257

--- _proyectos/Sillon002/sillon002.ino
+++ _proyectos/Sillon002/sillon002.ino
@@ -4,13 +4,15 @@
 #define PN HIGH                  // Cambiar para logica positiva/negativa
 
 /*
-Que solo funcione 1 cosa a la vez de las que consumen mucho
+Que solo funcione 1 cosa a la vez de las que consumen mucho, hecho casi todos los casos
 Tiempo de bacha y valor
 Posicion cero, salida paciente
 
 Los limites de carrera para seguridad
-Efecto rebote en los botones
-El agua carga por un tiempo determinado, que se pueda frenar.
+Efecto rebote en los botones. hecho parcialmente.
+El agua carga por un tiempo determinado
+  Que se pueda modificar el tiempo
+  que se pueda frenar.
 que cuente las vueltas
   y las tome como limite de carrera
 al menos 3 memorias para la posicion del sillon
@@ -27,12 +29,15 @@ const int inAsientoAbajo = 6; //Pulsador fisico
 const int inLampara = 5; //Pulsador fisico
 const int inVaso = 4; //Pulsador fisico
 const int inBacha = 3; //Pulsador fisico
+const int inPaciente0 = 2; //Pulsador fisico
 const int ouRespaldoAtras = A0; //Direccion izquireda
 const int ouRespaldoAdelante = A1; //Direccion derecha
 const int ouAsientoArriba = A2;  
 const int ouAsientoAbajo = A3;  
 const int ouLampara = A4;  
 const int ouVaso = A5;  
+const int ouBacha = A6;  
+const int ouPaciente0 = 13;  
 
 int valorPulsador = 0;  //variable intermedia para verificar si se oprimio un pulsador
 bool estadoLampara = OF ; //Estado de la iluminacion
@@ -40,10 +45,14 @@ int valorSalida;  //variable axiliar
 //bool algunPulsadorActivo = flase;  //variable axiliar
 
 //Definiciones para el manejo del vaso
-unsigned long T_Control=0;      // Para controlar el tiempo consumido
-unsigned long T_Apagar=3000;    // Tiempo de funcionamiento
-bool EstadoPin=PN; //Pulsador sin presionar           
-bool AnteriorEstadoPin=PN; //Pulsador sin presionar
+unsigned long T_ControlVaso=0;      // Para controlar el tiempo consumido
+unsigned long T_ControlBacha=0;      // Para controlar el tiempo consumido
+unsigned long T_ApagarVaso=5000;    // Tiempo de funcionamiento
+unsigned long T_ApagarBacha=15000;    // Tiempo de funcionamiento
+bool EstadoPinVaso=PN; //Pulsador sin presionar           
+bool EstadoPinBacha=PN; //Pulsador sin presionar           
+bool AnteriorEstadoPinVaso=PN; //Pulsador sin presionar
+bool AnteriorEstadoPinBacha=PN; //Pulsador sin presionar
 
 void setup() {
   // Define los pines de entrada
@@ -53,16 +62,19 @@ void setup() {
   pinMode(inAsientoAbajo, INPUT_PULLUP); 
   pinMode(inLampara, INPUT_PULLUP); 
   pinMode(inVaso, INPUT_PULLUP); 
-  // Define los pines de salida
+  pinMode(inBacha, INPUT_PULLUP); 
+  pinMode(inPaciente0, INPUT_PULLUP); 
+  // Definir los pines de salida
   pinMode(ouRespaldoAtras, OUTPUT);
   pinMode(ouRespaldoAdelante, OUTPUT);
   pinMode(ouAsientoArriba, OUTPUT);
   pinMode(ouAsientoAbajo, OUTPUT);
   pinMode(ouLampara, OUTPUT);
   pinMode(ouVaso, OUTPUT);
-  // incializamos estados necesarios
+  pinMode(ouBacha, OUTPUT);
+  //pinMode(ouPaciente0, OUTPUT); Este no es una salida espcifica es una combinacion de otras salidas
+  // incializar estados necesarios
   digitalWrite(ouLampara , LOW) ; // Apagamos Luz al comenzar
-  //digitalWrite(inVaso , ON) ; // Apagamos el vaso al comenzar
 }
 
 void loop() {
@@ -72,6 +84,8 @@ void loop() {
   VerificarPulsadorAsientoAbajo();
   VerificarPulsadorLampara();
   VerificarPulsadorVaso();
+  VerificarPulsadorBacha();
+  //VerificarPulsadorPaciente0(); A relaizar luego
 
 }
 
@@ -79,74 +93,119 @@ void VerificarPulsadorRespaldoAtras() {
   valorPulsador = digitalRead(inRespaldoAtras); //asignar estado del pulsador a una variable
   valorSalida   = ouRespaldoAtras;
    if (valorPulsador == PS) {
-    //algunPulsadorActivo = true
-    //if (digitalRead(ouRespaldoAdelante) == ON 
-    if (estadoLampara == ON) {
-      digitalWrite(ouLampara, LOW); //apagar lampara porque estaba prendida
+    if (digitalRead(ouRespaldoAdelante) == OF &&
+          digitalRead(ouAsientoAbajo) == OF &&
+          digitalRead(ouAsientoArriba) == OF){
+      if (estadoLampara == ON) {
+        digitalWrite(ouLampara, LOW); //apagar lampara porque estaba prendida
+      }
+      digitalWrite(valorSalida, HIGH);
     }
-    digitalWrite(valorSalida, HIGH);
   }
   else {
     digitalWrite(valorSalida, LOW);
-    digitalWrite(ouLampara, estadoLampara); //restaurar estado de lampara
   }
 }
 
 void VerificarPulsadorRespaldoAdelante() {
   valorPulsador = digitalRead(inRespaldoAdelante); //asignar estado del pulsador a una variable
   valorSalida   = ouRespaldoAdelante;
-   if (valorPulsador == PS) { 
-    if (estadoLampara == ON) {
-      digitalWrite(ouLampara, LOW); //apagar lampara porque estaba prendida
+   if (valorPulsador == PS) {
+    if (digitalRead(ouRespaldoAtras) == OF &&
+          digitalRead(ouAsientoAbajo) == OF &&
+          digitalRead(ouAsientoArriba) == OF){
+      if (estadoLampara == ON) {
+        digitalWrite(ouLampara, LOW); //apagar lampara porque estaba prendida
+      }
+      digitalWrite(valorSalida, HIGH);
     }
-    digitalWrite(valorSalida, HIGH);
   }
   else {
     digitalWrite(valorSalida, LOW);
-    digitalWrite(ouLampara, estadoLampara); //restaurar estado de lampara
   }
 }
 
 void VerificarPulsadorAsientoArriba() {
-  if (digitalRead(inAsientoArriba) == HIGH) {
-      digitalWrite(ouAsientoArriba, LOW);
+  valorPulsador = digitalRead(inAsientoArriba); //asignar estado del pulsador a una variable
+  valorSalida   = ouAsientoArriba;
+   if (valorPulsador == PS) {
+    if (digitalRead(ouRespaldoAtras) == OF &&
+          digitalRead(ouRespaldoAdelante) == OF &&
+          digitalRead(ouAsientoAbajo) == OF){
+      if (estadoLampara == ON) {
+        digitalWrite(ouLampara, LOW); //apagar lampara porque estaba prendida
+      }
+      digitalWrite(valorSalida, HIGH);
+    }
   }
   else {
-      digitalWrite(ouAsientoArriba, HIGH);
+    digitalWrite(valorSalida, LOW);
   }
 }
 
 void VerificarPulsadorAsientoAbajo() {
-  if (digitalRead(inAsientoAbajo) == HIGH) {
-      digitalWrite(ouAsientoAbajo, LOW);
+  valorPulsador = digitalRead(inAsientoAbajo); //asignar estado del pulsador a una variable
+  valorSalida   = ouAsientoAbajo;
+   if (valorPulsador == PS) {
+    if (digitalRead(ouRespaldoAtras) == OF &&
+          digitalRead(ouRespaldoAdelante) == OF &&
+          digitalRead(ouAsientoArriba) == OF){
+      if (estadoLampara == ON) {
+        digitalWrite(ouLampara, LOW); //apagar lampara porque estaba prendida
+      }
+      digitalWrite(valorSalida, HIGH);
+    }
   }
   else {
-      digitalWrite(ouAsientoAbajo, HIGH);
+    digitalWrite(valorSalida, LOW);
   }
 }
 
 void VerificarPulsadorLampara() {
   bool  valor =  digitalRead(inLampara) ;  //leemos el botón pulsado: false  =  HIGH
-  if ( valor == false )                    // esto es que han pulsado el botón
+  if ( valor == false)                    // esto es que han pulsado el botón
     {
       estadoLampara = ! estadoLampara ;                  // cambiamos el estado
       digitalWrite(ouLampara, estadoLampara) ;    // escribimos el nuevo valor
       delay(500) ; //Para suavizar el rebote
     }
+  if (estadoLampara == ON){
+    if (digitalRead(ouRespaldoAdelante) == OF &&
+          digitalRead(ouRespaldoAtras) == OF &&
+          digitalRead(ouAsientoAbajo) == OF &&
+          digitalRead(ouAsientoArriba) == OF){
+        digitalWrite(ouLampara, ON) ;
+    }
+  }
 }
 
 void VerificarPulsadorVaso() {
-  EstadoPin=digitalRead(inVaso);    // Leer pin
-  if (AnteriorEstadoPin != EstadoPin)   // Estado ha variado?
+  EstadoPinVaso=digitalRead(inVaso);    // Leer pin
+  if (AnteriorEstadoPinVaso != EstadoPinVaso)   // Estado ha variado?
   {
-    T_Control=millis()+T_Apagar;        // Valor de millis para apagar
+    T_ControlVaso=millis()+T_ApagarVaso;        // Valor de millis para apagar
     digitalWrite (ouVaso, ON);          // Encender         
   }
-  AnteriorEstadoPin=EstadoPin;
-  if (millis()>T_Control && T_Control>0)// Finalizo el tiempo y lo estamos controlando?
+  AnteriorEstadoPinVaso=EstadoPinVaso;
+  if (millis()>T_ControlVaso && T_ControlVaso>0)// Finalizo el tiempo y lo estamos controlando?
   {
     digitalWrite (ouVaso, OF);          // Apagar
-    T_Control=0;                        // Dejamos de controlar
+    T_ControlVaso=0;                        // Dejamos de controlar
+  }
+}
+
+void VerificarPulsadorBacha() {
+  EstadoPinBacha=digitalRead(inBacha);    // Leer pin
+  if (AnteriorEstadoPinBacha != EstadoPinBacha)   // Estado ha variado?
+  {
+    T_ControlBacha=millis()+T_ApagarBacha;        // Valor de millis para apagar
+    digitalWrite (ouBacha, ON);          // Encender         
+  }
+  AnteriorEstadoPinBacha=EstadoPinBacha;
+  if (millis()>T_ControlBacha && T_ControlBacha>0)// Finalizo el tiempo y lo estamos controlando?
+  {
+    digitalWrite (ouBacha, OF);          // Apagar
+    T_ControlBacha=0;                        // Dejamos de controlar
   }
 }
 
